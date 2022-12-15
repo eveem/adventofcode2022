d08ec0de004c1be52121ff914af21e1cc7174035
eveem <monpriya.tammavong@gmail.com>
2022-12-15 10:52:23 +0700

    finish day13 part2

--- day13/solution.py
+++ day13/solution.py
@@ -1,4 +1,6 @@
 from ast import literal_eval
+from functools import cmp_to_key
+from struct import pack
 
 # f = open("small_input.txt")
 f = open("input.txt")
@@ -31,4 +33,28 @@ for i in range(0, n, 3):
     total += pair
   pair += 1
 
-print(total)
+print("PART 1:", total)
+
+packets = [literal_eval(item) for item in lines if item != ""]
+packets.append([[2]])
+packets.append([[6]])
+
+n = len(packets)
+
+for i in range(n):
+  min_idx = i
+  for j in range(i + 1, n):
+    status = cmp(packets[min_idx], packets[j])
+    if status == False:
+      min_idx = j
+  packets[i], packets[min_idx] = packets[min_idx], packets[i]
+
+expected = open("expected.txt")
+
+# print(all([literal_eval(e.strip()) == p for p, e in zip(packets, expected.readlines())]))
+
+idx2 = packets.index([[2]]) + 1
+idx6 = packets.index([[6]]) + 1
+
+# print(idx2, idx6, idx2 * idx6)
+print("PART 2:", idx2 * idx6)
